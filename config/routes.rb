d02a78ffcc768534a8ad323dff94517cd3ad34ef
Christopher Hildebrandt <christopher_leighton@icloud.com>
2019-06-19 20:18:46 +1000

    Added name column to user tables, include name attribut to sign_up_params and account_update_params, methods inside the Registration Controller included

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # define 'pages#index' route, so when we open our root page, Rails knows which controller and its action to call.
   root to: 'pages#index'
 end
 
-devise_scope:user do
-  get 'login',to:'devise/sessions#new'
-end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_093710) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
